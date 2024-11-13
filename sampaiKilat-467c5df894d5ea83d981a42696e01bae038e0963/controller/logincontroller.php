bb629d782a0b2d4b151e2cf39dffa5fa2cc77d4c
Dar1786 <145871011+Dar1786@users.noreply.github.com>
2024-11-13 20:10:50 +0700

    ditambahin hash verif ama prepared statement

--- sampaiKilat-467c5df894d5ea83d981a42696e01bae038e0963/controller/logincontroller.php
+++ sampaiKilat-467c5df894d5ea83d981a42696e01bae038e0963/controller/logincontroller.php
@@ -3,34 +3,40 @@ session_start();
 
 include './koneksiDB.php'; 
 
-// Memeriksa apakah user mengirimkan form via POST
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $username = htmlspecialchars($_POST['username']);
     $password = htmlspecialchars($_POST['password']);
     
-    // Membersihkan input untuk mencegah SQL Injection
     $usernamebersih = mysqli_real_escape_string($conn, $username);
     $passwordbersih = mysqli_real_escape_string($conn, $password);
+    $salt="SampaiKilat";
+    $passwordbersih=md5(md5(md5($passwordbersih).$salt));
+    echo $passwordbersih;
 
-    // Query untuk memeriksa apakah username dan password ada di database
-    $sql = "SELECT * FROM testing WHERE username='$usernamebersih' AND pass='$passwordbersih'";
-    $kueri = $conn->query($sql);
 
-    // Jika hasil query lebih dari 0 baris, login berhasil
-    if ($kueri->num_rows > 0) {
-        $row = $kueri->fetch_assoc(); // Mengambil data user dari hasil query
+   
+    $stmt = $conn->prepare("SELECT * FROM testing WHERE username = ? AND pass = ?");
+    $stmt->bind_param("ss",$usernamebersih,$passwordbersih);
+    $stmt->execute();
+    $result=$stmt->get_result();
+    
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc(); // Mengambil data user dari hasil query
 
         // Menyimpan data penting ke dalam session
         $_SESSION['username'] = $row['username']; // Simpan username ke session
-        $_SESSION['session_id'] = session_id(); // Simpan session ID
+        $_SESSION['session_id'] = session_id(); // Simpan session 
         
-        // Redirect ke halaman utama setelah login
+       
         header("Location: ../homepage.html");
-        exit(); // Menghentikan script setelah redirect
-
+        exit(); 
     } else {
-        // Jika username atau password salah
-        echo "Username atau password salah!";
+        
+        header("Location:../src/login_admin/loginpage");
+        exit();
+       
+        
+       
     }
 
 } else {
